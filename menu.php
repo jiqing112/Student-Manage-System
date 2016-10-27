917106d0fe2738cdbb72bdb361ed985db7690297
言之灵 <ji_qing@outlook.com>
2016-10-27 15:39:59 +0800

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>学生信息管理</h2>
+<a href="index.php">浏览学生</a>
+<a href="add.php">添加学生</a>
+<hr>
+
+</body>
+</html>
+
+<?php
+
+?>
